92993cd40714997f26c872df690a9f23473601cf
Philip HARRELL <alightedlamp@gmail.com>
2017-03-24 21:19:20 -0500

    refactor

---
+++ app.js
@@ -0,0 +1,11 @@
+require.config({
+  paths: {
+    "jquery": "lib/jquery",
+    "underscore": "lib/underscore",
+    "data": "lib/data"
+  }
+});
+
+require(['lib/modules/gallery' ,'lib/modules/nav'], function() {
+
+});
