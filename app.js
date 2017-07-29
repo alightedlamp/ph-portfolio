4c0a3d3abb5077339c1e51b7e7997e824445e576
Philip Harrell <alightedlamp@gmail.com>
2017-07-28 21:20:21 -0500

    add wordpress blog feed

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ require.config({
   paths: {
     "jquery": "lib/jquery",
     "underscore": "lib/underscore",
-    "data": "lib/data"
+    "data": "lib/data",
+    "blog": "lib/modules/blog"
   }
 });
 
-require(['jquery', 'underscore', 'data', 'lib/modules/nav', 'lib/modules/gallery'],
+require(['jquery', 'underscore', 'data', 'lib/modules/nav', 'lib/modules/gallery', 'blog'],
     function($, _, data, nav, gallery) {
 
     // Prevents css transitions until window load
@@ -37,5 +38,16 @@ require(['jquery', 'underscore', 'data', 'lib/modules/nav', 'lib/modules/gallery
     });
     $("#next-card").on("click", function() {
         cardView.showNext();
+    });
+
+    var blogEntries = getEntries(url, function(response) {
+        var entries = JSON.parse(response);
+        entries = entries.posts;
+        for (var i = 0; i < 2; i++) {
+            $("#blog-posts").append("<h3>" + entries[i].title + "</h3>");
+            $("#blog-posts").append("<h4><small>" + entries[i].date.split("T")[0] + "</small></h4>");
+            $("#blog-posts").append("<p>" + entries[i].excerpt + "</p>");
+            $("#blog-posts").append("<a href='" + entries[i].URL + "' target='_blank'>Read more</a>");
+        }
     });
 });
