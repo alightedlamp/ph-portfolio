3009f7cb25a79c98b4fe0d6eddfeca9421f703b9
Philip Harrell <alightedlamp@gmail.com>
2017-10-10 22:14:12 -0500

    Spell sandwhich correctly

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,8 @@ export const INTROS = [
   'makes things.',
   'Painter, programmer, producer',
   'dot com',
-  'is eating a sandwhich.',
-  'male/29/atx',
-  'where am i?'
+  'is eating a sandwich.',
+  '29/male/atx',
+  'where am i?',
+  'Hello, World!'
 ];
