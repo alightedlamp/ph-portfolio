89d3167ffc81211d4957ff274ca2bb36e6c441bd
Philip Harrell <alightedlamp@gmail.com>
2018-01-16 20:46:22 -0600

    Cleanup mobile styles and fix stray className

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -5,7 +5,7 @@ class Contact extends Component {
     return (
       <div id="contact" className="bg-washed-blue black ph5 pv5 center">
         <div className="content">
-          <h2 class="f5 ttu mb3">Contact</h2>
+          <h2 className="f5 ttu mb3">Contact</h2>
           <p className="mb2">
             For general inquiries, please{' '}
             <a
