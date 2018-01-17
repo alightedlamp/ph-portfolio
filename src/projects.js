fb07283a9099a2fd3558d70731abdacaf4c261e1
Philip Harrell <alightedlamp@gmail.com>
2018-01-17 07:37:10 -0600

    Spellcheck

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,8 @@ import giftastic_img from './assets/img/project_giftastic.png';
 import rps_img from './assets/img/project_rps.png';
 import thestudio_img from './assets/img/project_thestudio.png';
 
-export const projects = [{
+export const projects = [
+  {
     name: 'Stranger Hangman',
     description: 'A vanilla JS Stranger Things themed hangman game.',
     preview_img: hangman_img,
@@ -28,11 +29,13 @@ export const projects = [{
   },
   {
     name: 'Monique',
-    description: 'Monique is a bot built with NodeJS that responds to voice commands. She aspires to be like Alexa one day.',
+    description:
+      'Monique is a bot built with NodeJS that responds to voice commands. She aspires to be like Alexa one day.',
     github_url: 'https://github.com/alightedlamp/Monique'
   }
 ];
-export const art = [{
+export const art = [
+  {
     name: 'JSART.ART',
     description: 'A collection of JavaScript-based drawing applications.',
     preview_img: jsart_img,
@@ -41,16 +44,17 @@ export const art = [{
   },
   {
     name: 'CSSART.CLUB',
-    description: 'A collection of subtly interactive digital paintings made with HTML and CSS.',
+    description:
+      'A collection of subtly interactive digital paintings made with HTML and CSS.',
     preview_img: cssart_img,
     github_url: 'https://github.com/alightedlamp/CSSART.CLUB',
     url: 'https://cssart.club'
   },
   {
     name: 'The Studio',
-    description: 'An aphemeral abstract painting generator.',
+    description: 'An ephemeral abstract painting generator.',
     preview_img: thestudio_img,
     github_url: 'https://github.com/alightedlamp/the-studio',
-    url: 'https://alightedlamp.github.io/the-studio/',
-  },
+    url: 'https://alightedlamp.github.io/the-studio/'
+  }
 ];
